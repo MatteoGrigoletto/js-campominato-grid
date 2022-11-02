60e666ecaafc1aecdaee06f2f9bdfd236104c535
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-02 16:36:07 +0100

    Fine

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 `use strict`;
 
-function generator(location,number,modifyClass){
+function generator(location,number,divSize,bgColor){
 
     for(i = 1; i <= number; i++){          
         let cellBlock = document.createElement(`div`);
-        cellBlock.classList.add(modifyClass)
+        cellBlock.classList.add(divSize);
         cellBlock.innerHTML = i;
         location.append(cellBlock);
         cellBlock.addEventListener(`click`, function(){
-            this.classList.toggle(`bg-blue`);
+            this.classList.toggle(bgColor);
             console.log(`hai cliccato la casella `, this.innerHTML);
         })
     }
@@ -21,10 +21,10 @@ const Generator = btnGenerator.addEventListener(`click`, function(){
     const difficulty = document.getElementById(`difficulty`).value;
     container.innerHTML = ``;
         if(difficulty === `easy`){ 
-        generator(container, 100, `div-10`);     
+        generator(container, 100, `div-10`,`bg-blue`);     
         }else if(difficulty === `normal`){
-            generator(container,81, `div-9`)
+            generator(container,81, `div-9`,`bg-blue`);
         }else{
-            generator(container,49,`div-7`)
+            generator(container,49,`div-7`,`bg-blue`);
         }
 });
