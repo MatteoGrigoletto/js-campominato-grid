fe0b554588732d3cf2353633018d19e0cecd1962
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-02 15:28:57 +0100

    Aggiunge primo ciclo con creazione di 100 div

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 `use strict`;
 
+const container = document.querySelector(`.container`);
+const btnGenerator = document.getElementById(`my-btn`);
+const Generator = btnGenerator.addEventListener(`click`, function(){
+
+
+    container.innerHTML = ``;
+    for(i = 1; i <= 100; i++){
+        
+        let cellBlock = document.createElement(`div`);
+        cellBlock.classList.add(`div-10`)
+        cellBlock.innerHTML = i;
+        container.append(cellBlock);
+
+    }
+
+
+
+
+
+
+
+});
