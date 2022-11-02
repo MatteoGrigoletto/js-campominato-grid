7205fe56b8f20cb36710bbf9f0c09572d2ba0580
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-02 15:52:54 +0100

    Create diverse griglie in base alla difficolta'

--- js/main.js
+++ js/main.js
@@ -1,20 +1,47 @@
 `use strict`;
 
+
 const container = document.querySelector(`.container`);
 const btnGenerator = document.getElementById(`my-btn`);
 const Generator = btnGenerator.addEventListener(`click`, function(){
 
-
+    const difficulty = document.getElementById(`difficulty`).value;
     container.innerHTML = ``;
-    for(i = 1; i <= 100; i++){
+    if(difficulty === `easy`){
         
-        let cellBlock = document.createElement(`div`);
-        cellBlock.classList.add(`div-10`)
-        cellBlock.innerHTML = i;
-        container.append(cellBlock);
-        cellBlock.addEventListener(`click`, function(){
-            this.classList.toggle(`bg-blue`);
-            console.log(`hai cliccato la casella `, this.innerHTML);
-        })
+        for(i = 1; i <= 100; i++){          
+            let cellBlock = document.createElement(`div`);
+            cellBlock.classList.add(`div-10`)
+            cellBlock.innerHTML = i;
+            container.append(cellBlock);
+            cellBlock.addEventListener(`click`, function(){
+                this.classList.toggle(`bg-blue`);
+                console.log(`hai cliccato la casella `, this.innerHTML);
+            })
+        }
+    }else if(difficulty === `normal`){
+        
+        for(i = 1; i <= 81; i++){          
+            let cellBlock = document.createElement(`div`);
+            cellBlock.classList.add(`div-9`)
+            cellBlock.innerHTML = i;
+            container.append(cellBlock);
+            cellBlock.addEventListener(`click`, function(){
+                this.classList.toggle(`bg-blue`);
+                console.log(`hai cliccato la casella `, this.innerHTML);
+            })
+        }       
+    }else{
+
+        for(i = 1; i <= 49; i++){          
+            let cellBlock = document.createElement(`div`);
+            cellBlock.classList.add(`div-7`)
+            cellBlock.innerHTML = i;
+            container.append(cellBlock);
+            cellBlock.addEventListener(`click`, function(){
+                this.classList.toggle(`bg-blue`);
+                console.log(`hai cliccato la casella `, this.innerHTML);
+            })
+        } 
     }
 });
